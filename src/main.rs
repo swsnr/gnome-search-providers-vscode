f2dba024644fee1807c17007c7dcec2a074b5d4f
Vianney le Clément de Saint-Marcq <code@quartic.eu>
2021-11-23 22:00:10 +0100

    Refactor JSON entries parsing
    Use an untagged enum to cover the various formats of the `entries`
    array.  As we are not interested in individual files, the `fileUri`
    field has been removed and replaced by a generic `Other` variant that
    also covers any future VSCode changes.

--- src/main.rs
+++ src/main.rs
@@ -28,11 +28,23 @@ use gnome_search_provider_common::mainloop::*;
 use gnome_search_provider_common::matching::*;
 
 #[derive(Debug, Deserialize)]
-struct StorageOpenedPathsListEntry {
-    #[serde(rename = "folderUri")]
-    folder_uri: Option<String>,
-    #[serde(rename = "fileUri")]
-    file_uri: Option<String>,
+#[serde(untagged)]
+enum StorageOpenedPathsListEntry {
+    Folder {
+        #[serde(rename = "folderUri")]
+        uri: String,
+    },
+    Other(serde_json::Value),
+}
+
+impl StorageOpenedPathsListEntry {
+    /// Move this entry into a workspace URL.
+    fn into_workspace_url(self) -> Option<String> {
+        match self {
+            Self::Folder { uri } => Some(uri),
+            Self::Other(_) => None,
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -74,7 +86,7 @@ impl Storage {
             let workspaces3 = paths.workspaces3.unwrap_or_default();
             entries
                 .into_iter()
-                .filter_map(|entry| entry.folder_uri)
+                .filter_map(|entry| entry.into_workspace_url())
                 .chain(workspaces3.into_iter())
                 .collect()
         } else {
